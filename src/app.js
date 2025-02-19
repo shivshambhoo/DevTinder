fd6397fc52ab17e46e54b3b77095fb31274a74a1
shivshambhoo chaudhary <shivshambhookch@gmail.com>
2025-02-20 00:28:26 +0530

    changes done

--- src/app.js
+++ src/app.js
@@ -3,16 +3,24 @@ const dotenv = require("dotenv");
 const process=require('process');
 const dbConnect=require("./config/database")
 
-const app=express();
+dotenv.config();
 
+// import routes
+const userRoute=require("./routes/userRoutes");
+
+const app=express();
+app.use(express.json());
 const port=process.env.WEBSITE_PORT||8000;
+
+dbConnect();
+
+// routes 
+app.use('/api/v1',userRoute);
 
 app.use((req,res)=>{
     res.send('Hello from Express!')
 })
 
-dotenv.config();
-dbConnect();
 app.listen(port,()=>{
     console.log(`server running on ${port}`);
 })
